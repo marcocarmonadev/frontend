2d0d122490f1ac63c7d176ff04d5625513a1645a
Marco Carmona <marcocarmonadev@outlook.com>
2024-06-23 03:54:54 +0000

    Update statistics organism

--- source/frameworks_and_devices/user_interface/organisms/statistics.py
+++ source/frameworks_and_devices/user_interface/organisms/statistics.py
@@ -5,7 +5,7 @@ from frameworks_and_devices.external_interfaces import get_backend_client
 def display():
     backend_client = get_backend_client()
     entries_statistics = backend_client.get_entries_statistics()
-    column1, column2, column3 = st.columns(3)
+    column1, column2, column3, column4 = st.columns(4)
 
     column1.metric(
         label="Income amount",
@@ -18,6 +18,11 @@ def display():
     )
 
     column3.metric(
+        label="Complete expense amount",
+        value=f'${entries_statistics["complete_expense_amount"]}',
+    )
+
+    column4.metric(
         label="Total amount",
         value=f'${entries_statistics["total_amount"]}',
     )
